a4ee355e3a599781042a281d95e37b03ca054e1f
saneery <anatolyniky@gmail.com>
2019-05-28 19:33:36 +0300

    set onclick tags in async loaded elements to open modal windows

--- apps/block_scout_web/assets/js/lib/modals.js
+++ apps/block_scout_web/assets/js/lib/modals.js
@@ -6,11 +6,6 @@ $(function () {
     $('#becomeCandidateModal').modal()
   })
 
-  $('.js-validator-info-modal').on('click', function (e) {
-    var id = e.currentTarget.id.split("_")[1];
-    $('#validatorInfoModal' + id).modal()
-  })
-
   $('.js-move-stake').on('click', function () {
     $('#errorStatusModal').modal()
   })
@@ -22,58 +17,60 @@ $(function () {
   $('.js-copy-address').on('click', function () {
     $('#successStatusModal').modal()
   })
-
-  $('.js-stake-stake').on('click', function (e) {
-    var id = e.currentTarget.id.split("_")[1];
-    const modal = '#stakeModal' + id;
-    const progress = parseInt($(`${modal} .js-stakes-progress-data-progress`).text())
-    const total = parseInt($(`${modal} .js-stakes-progress-data-total`).text())
-
-    $(modal).modal()
-
-    setupStakesProgress(progress, total, modal)
-  })
-
-  $('.js-withdraw-stake').on('click', function (e) {
-    var id = e.currentTarget.id.split("_")[1];
-    const modal = '#withdrawModal' + id;
-    const progress = parseInt($(`${modal} .js-stakes-progress-data-progress`).text())
-    const total = parseInt($(`${modal} .js-stakes-progress-data-total`).text())
-
-    $(modal).modal()
-
-    setupStakesProgress(progress, total, modal)
-  })
-
-  function setupStakesProgress (progress, total, modal) {
-    const stakeProgress = $(`${modal} .js-stakes-progress`)
-    const primaryColor = $('.btn-full-primary').css('background-color')
-    const backgroundColors = [
-      primaryColor,
-      'rgba(202, 199, 226, 0.5)'
-    ]
-    const progressBackground = total - progress
-
-    // eslint-disable-next-line no-unused-vars
-    let myChart = new Chart(stakeProgress, {
-      type: 'doughnut',
-      data: {
-        datasets: [{
-          data: [progress, progressBackground],
-          backgroundColor: backgroundColors,
-          hoverBackgroundColor: backgroundColors,
-          borderWidth: 0
-        }]
-      },
-      options: {
-        cutoutPercentage: 80,
-        legend: {
-          display: false
-        },
-        tooltips: {
-          enabled: false
-        }
-      }
-    })
-  }
 })
+
+window.openValidatorInfoModal = function(id) {
+  $('#validatorInfoModal' + id).modal()
+}
+
+window.openStakeModal = function(id) {
+  const modal = '#stakeModal' + id;
+  const progress = parseInt($(`${modal} .js-stakes-progress-data-progress`).text())
+  const total = parseInt($(`${modal} .js-stakes-progress-data-total`).text())
+
+  $(modal).modal()
+
+  setupStakesProgress(progress, total, modal)
+}
+
+window.openWithdrawModal = function(id) {
+  const modal = '#withdrawModal' + id;
+  const progress = parseInt($(`${modal} .js-stakes-progress-data-progress`).text())
+  const total = parseInt($(`${modal} .js-stakes-progress-data-total`).text())
+
+  $(modal).modal()
+
+  setupStakesProgress(progress, total, modal)
+}
+
+function setupStakesProgress (progress, total, modal) {
+  const stakeProgress = $(`${modal} .js-stakes-progress`)
+  const primaryColor = $('.btn-full-primary').css('background-color')
+  const backgroundColors = [
+    primaryColor,
+    'rgba(202, 199, 226, 0.5)'
+  ]
+  const progressBackground = total - progress
+
+  // eslint-disable-next-line no-unused-vars
+  let myChart = new Chart(stakeProgress, {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+        data: [progress, progressBackground],
+        backgroundColor: backgroundColors,
+        hoverBackgroundColor: backgroundColors,
+        borderWidth: 0
+      }]
+    },
+    options: {
+      cutoutPercentage: 80,
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false
+      }
+    }
+  })
+}
